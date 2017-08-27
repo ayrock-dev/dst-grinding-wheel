2ed07ac9bba5591c342caca7d566ac8eb7e52018
ayrock-dev <ayrocklee@gmail.com>
2017-08-27 15:14:54 -0500

    added code to give the desired items "sharpenable"

--- modmain.lua
+++ modmain.lua
@@ -15,6 +15,8 @@ Recipe 		= GLOBAL.Recipe
 TECH		= GLOBAL.TECH
 Ingredient 	= GLOBAL.Ingredient
 
+-- SETUP OF THE GRINDING WHEEL
+
 local grinding_wheel = Recipe(
         "grinding_wheel",
         {
@@ -28,3 +30,32 @@ local grinding_wheel = Recipe(
 
 grinding_wheel.atlas = "images/inventoryimages/grinding_wheel.xml"
 AddMinimapAtlas("images/inventoryimages/grinding_wheel.xml")
+
+-- ADD ITEMS WHICH WE WANT TO BE SHARPENABLE
+
+local function AddSharpenableComponent(prefab)
+    if not prefab.components.insulator then
+        prefab:AddComponent("sharpenable");
+    end
+end
+
+local sharpenable_items = {
+    "axe",
+    "goldenaxe",
+    "pickaxe",
+    "goldenpickaxe",
+    "shovel",
+    "goldenshovel",
+    "pitchfork",
+    "hammer",
+    "razor",
+    "spear",
+    "tentaclespike",
+    "ruins_bat",
+    "spear_wathgrithr",
+    "multitool_axe_pickaxe",
+}
+
+for _,item in sharpenable_items do
+    AddPrefabPostInit(item, AddSharpenableComponent);
+end
