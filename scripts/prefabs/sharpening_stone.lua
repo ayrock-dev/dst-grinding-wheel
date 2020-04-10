edd75892c7f6ee007462f6fa1697cb7b00f76a90
Eric Lee <ayrocklee@gmail.com>
2020-04-09 17:14:52 -0700

    Add sharpener component and sharpening_stone prefab

---
+++ scripts/prefabs/sharpening_stone.lua
@@ -0,0 +1,50 @@
+local assets =
+{
+    Asset("ANIM", "anim/sharpening_stone.zip"),
+    Asset("ATLAS", "images/inventoryimages/sharpening_stone.xml"),
+    Asset("IMAGE", "images/inventoryimages/sharpening_stone.tex"),
+}
+
+local function onsharpen(inst, target, doer) 
+    --doer:PushEvent("repair")
+end
+
+local function fn()
+    local inst = CreateEntity()
+
+    inst.entity:AddTransform()
+    inst.entity:AddAnimState()
+    inst.entity:AddNetwork()
+
+    MakeInventoryPhysics(inst)
+
+    inst.AnimState:SetBank("sharpening_stone")
+    inst.AnimState:SetBuild("sharpening_stone")
+    inst.AnimState:PlayAnimation("idle")
+
+    inst.entity:SetPristine()
+
+    if not TheWorld.ismastersim then
+        return inst
+    end
+
+    inst:AddComponent("sharpener")
+    inst.components.sharpener.onsharpen = onsharpen;
+
+    inst:AddComponent("finiteuses")
+    inst.components.finiteuses:SetMaxUses(5)
+    inst.components.finiteuses:SetUses(5)
+    inst.components.finiteuses:SetOnFinished(inst.Remove)
+
+    inst:AddComponent("inspectable")
+
+    inst:AddComponent("inventoryitem")
+    inst.components.inventoryitem.atlasname = "images/inventoryimages/sharpening_stone.xml"
+    inst.components.inventoryitem:SetSinks(true)
+
+    MakeHauntableLaunch(inst)
+
+    return inst
+end
+
+return Prefab("sharpening_stone", fn, assets)
